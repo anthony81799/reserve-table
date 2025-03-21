f5b6e78fbe03f481a6243b811b956f76f9fb2626
Anthony Mason <anthony81799@gmail.com>
2025-03-21 08:19:57 -0400

    Semantic Structure.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import Nav from "./Nav";
+import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<>
+			<Header />
+			<Nav />
+			<Main />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
